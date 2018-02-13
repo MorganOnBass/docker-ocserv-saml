173770bd892094ef891bd8160e79235685319fd9
MarkusMcNugen <marknewton5@gmail.com>
2018-02-12 20:38:36 -0500

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -110,16 +110,23 @@ elif [[ ${TUNNEL_MODE} == "split-include" ]]; then
 		# process name servers in the list
 		for tunnel_route_item in "${tunnel_route_list[@]}"; do
 			tunnel_route_item=$(echo "${tunnel_route_item}" | sed -e 's~^[ \t]*~~;s~[ \t]*$~~')
-			echo "${tunnel_route_item}"
-			IP=$(ipcalc -b ${tunnel_route_item} | awk '/Address/ {print $2; exit}')
-			echo "$IP"
-			NETMASK=$(ipcalc -b ${tunnel_route_item} | awk '/Netmask/ {print $2; exit}')
-			echo "$NETMASK"
-			TUNDUP=$(cat /config/ocserv.conf | grep "route=${IP}/${NETMASK}")
-			if [[ -z "$TUNDUP" ]]; then
-				echo "$(date) [info] Adding route=$IP/$NETMASK to ocserv.conf"
-				echo "route=$IP/$NETMASK" >> /config/ocserv.conf
+			IFS='/' read -ra ip_subnet_list <<< "${tunnel_route_item}"
+			STRLENGTH=$(echo -n ${ip_subnet_list[1]} | wc -m)
+			echo "Subnet length = ${STRLENGTH}"
+			if [[ $STRLENGTH > "2" ]]; then
+				IP=$(sipcalc ${ip_subnet_list[0]} ${ip_subnet_list[1]} | awk '/Host address/ {print $4; exit}')
+				NETMASK=$(sipcalc ${ip_subnet_list[0]} ${ip_subnet_list[1]} | awk '/Network mask/ {print $4; exit}')
+			else
+				IP=$(ipcalc -b ${tunnel_route_item} | awk '/Address/ {print $2}')
+				NETMASK=$(ipcalc -b ${tunnel_route_item} | awk '/Netmask/ {print $2}')
 			fi
+			#IP=$(ipcalc -b ${tunnel_route_item} | awk '/Address/ {print $2; exit}')
+			#NETMASK=$(ipcalc -b ${tunnel_route_item} | awk '/Netmask/ {print $2; exit}')
+			#TUNDUP=$(cat /config/ocserv.conf | grep "route=${IP}/${NETMASK}")
+			#if [[ -z "$TUNDUP" ]]; then
+			#	echo "$(date) [info] Adding route=$IP/$NETMASK to ocserv.conf"
+			#	echo "route=$IP/$NETMASK" >> /config/ocserv.conf
+			#fi
 		done
 	fi
 fi
