75796e91bc2664fb9e1d602c4406a742c3b2b548
MarkusMcNugen <marknewton5@gmail.com>
2018-02-12 20:43:54 -0500

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -113,11 +113,11 @@ elif [[ ${TUNNEL_MODE} == "split-include" ]]; then
 			IFS='/' read -ra ip_subnet_list <<< "${tunnel_route_item}"
 			STRLENGTH=$(echo -n ${ip_subnet_list[1]} | wc -m)
 			if [[ $STRLENGTH > "2" ]]; then
-				echo "$(date) Full subnet mask detected in route ${tunnel_route_item}"
+				echo "$(date) [info] Full subnet mask detected in route ${tunnel_route_item}"
 				IP=$(sipcalc ${ip_subnet_list[0]} ${ip_subnet_list[1]} | awk '/Host address/ {print $4; exit}')
 				NETMASK=$(sipcalc ${ip_subnet_list[0]} ${ip_subnet_list[1]} | awk '/Network mask/ {print $4; exit}')
 			else
-				echo "$(date) CIDR submet mask detected in route ${tunnel_route_item}"
+				echo "$(date) [info] CIDR submet mask detected in route ${tunnel_route_item}"
 				IP=$(ipcalc -b ${tunnel_route_item} | awk '/Address/ {print $2}')
 				NETMASK=$(ipcalc -b ${tunnel_route_item} | awk '/Netmask/ {print $2}')
 			fi
